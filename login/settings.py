0e40e5296976053ea7caf80b4352dab70d09a6e5
thelight21313 <artemijpopkov6@gmail.com>
2025-12-19 21:48:47 +0400

    В рот я ебал докер

--- login/settings.py
+++ login/settings.py
@@ -113,3 +113,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/login'
+
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
