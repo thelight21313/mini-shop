ff7dd661bbbd0f9ba4e242eb2004cdee57588df4
thelight21313 <artemijpopkov6@gmail.com>
2026-01-15 22:26:09 +0400

    Я опять все просрал

--- login/settings.py
+++ login/settings.py
@@ -38,7 +38,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'ratelimit.middleware.RatelimitMiddleware',
 ]
 
 ROOT_URLCONF = 'login.urls'
