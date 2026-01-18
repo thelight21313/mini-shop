ddb1ee6db2374a8e2858c1b4de074144cd42ce92
thelight21313 <artemijpopkov6@gmail.com>
2026-01-18 14:38:43 +0400

    add change category in API

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Account, Product, Cart, Wishlist
+from .models import Account, Product, Cart, Wishlist, Category
 
 # 1. Простой способ регистрации
 admin.site.register(Account)
 admin.site.register(Product)
 admin.site.register(Cart)
 admin.site.register(Wishlist)
+admin.site.register(Category)
