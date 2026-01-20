923b46f0830204fa103989f12cfd0fa6e256ccf6
thelight21313 <artemijpopkov6@gmail.com>
2026-01-20 22:38:44 +0400

    fix

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,1 @@
 from django.contrib import admin
-from .models import Product, Cart, Wishlist, Category
-
-
-admin.site.register(Product)
-admin.site.register(Cart)
-admin.site.register(Wishlist)
-admin.site.register(Category)
